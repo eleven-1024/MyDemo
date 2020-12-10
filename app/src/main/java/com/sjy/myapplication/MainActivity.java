f5d646a421c15382634a6a9ff28d3d8b315310ad
sjy <dazzling448214104@163.com>
2020-12-10 17:06:46 +0800

    werfghjkl;

--- app/src/main/java/com/sjy/myapplication/MainActivity.java
+++ app/src/main/java/com/sjy/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hhhhh", Toast.LENGTH_SHORT).show();
+        System.out.println("刘楠大傻逼");
     }
 }
