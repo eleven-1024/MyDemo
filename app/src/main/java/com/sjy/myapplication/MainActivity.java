40b8fb6254f971afaf4183e814d982ca91880eac
sjy <dazzling448214104@163.com>
2020-12-10 16:58:25 +0800

    werfghjkl;

--- app/src/main/java/com/sjy/myapplication/MainActivity.java
+++ app/src/main/java/com/sjy/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.sjy.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Toast.makeText(this, "hhhhh", Toast.LENGTH_SHORT).show();
     }
 }
